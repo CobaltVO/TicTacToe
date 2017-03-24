dd50de4e90ae94f334715bd3d14e1cd3a801f8a1
vladislavsumin <vladislavsumin@mail.ru>
2017-03-24 15:33:30 +0300

    Improved

--- 1java/src/ru/cobalt/Main.java
+++ 1java/src/ru/cobalt/Main.java
@@ -3,6 +3,5 @@ package ru.cobalt;
 public class Main {
     public static void main(String[] args) {
         TicTacToe t = new TicTacToe();
-        t.takeData();
     }
 }
--- 1java/src/ru/cobalt/TicTacToe.java
+++ 1java/src/ru/cobalt/TicTacToe.java
@@ -2,19 +2,26 @@ package ru.cobalt;
 
 import java.util.Scanner;
 
-public class TicTacToe {
+class TicTacToe {
     private char map[][] = new char[3][3]; // for storing marks
-    private char mark;
-    private int pos;
     private int count = 0;
+    private char currentPlayer = 'X';
 
-    TicTacToe () {
+    TicTacToe() {
         clearMap();
         startGameMessage();
         showMap();
+        takeData();
     }
 
-    public void clearMap () {
+    private void swapCurrentPlayer() {
+        if (currentPlayer == 'X')
+            currentPlayer = 'O';
+        else
+            currentPlayer = 'X';
+    }
+
+    private void clearMap() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 map[i][j] = '_';
@@ -22,8 +29,8 @@ public class TicTacToe {
         }
     }
 
-    public void showMap () {
-        for (int i= 0; i < 3; i++) {
+    private void showMap() {
+        for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 System.out.print(map[i][j] + " ");
             }
@@ -32,106 +39,106 @@ public class TicTacToe {
         System.out.println();
     }
 
-    public void startGameMessage () {
+    private void startGameMessage() {
         System.out.println("Greetings! This is TicTacToe game");
-        System.out.println("There are allowed marks: X, O; There are allowed positions: 1, 2, 3, 4, 5, 6, 7, 8, 9");
+        System.out.println("There are allowed positions: 1, 2, 3, 4, 5, 6, 7, 8, 9");
         System.out.println("First player begins game with X mark");
-        System.out.println("Type data in right order: firstly mark then position, e.g.: X 3");
+        System.out.println("Type position, e.g.: 3");
     }
 
-    public void setMark (char mark, int pos) {
-        if ((pos > 9) || (pos < 1)) {
-            System.err.println("Wrong position!\nType data again: ");
+    private void setMark(int pos) {
+        pos -= 1; // 0, 1, 2, 3, 4, 5, 6, 7, 8
+        int i = pos / 3;
+        int j = pos % 3;
+        if (map[i][j] == '_') {
+            map[i][j] = currentPlayer;
+        } else {
+            System.out.println("This place has been already taken\nChoose another one");
             takeData();
+            return;
         }
-        else if ((mark == 'X') || (mark == 'O') || (mark == 'x') || (mark == 'o')) {
-            pos -= 1; // 0, 1, 2, 3, 4, 5, 6, 7, 8
-            int i = pos / 3;
-            int j = pos % 3;
-            if (map[i][j] == '_') {
-                map[i][j] = mark;
-            } else {
-                System.out.println("This place has been already taken\nChoose another one");
-                takeData();
+        if (count > 3) {
+            if (didPlayerWin(currentPlayer, pos)) {
+                System.out.println("Player " + currentPlayer + " won!!!");
+                showMap();
                 return;
             }
-            if (count > 3) {
-                if (didPlayerWin(mark, pos)) {
-                    System.out.println("Player " + mark + " won!!!");
-                    showMap();
-                    return;
-                }
+        }
 
-            }
-            showMap();
-            if (count < 8) {
-                count++;
-                takeData();
-            } else System.out.println("Draw");
-        }
-        else {
-            System.err.println("Wrong symbol\nType data again: ");
+        showMap();
+        if (count < 8) {
+            count++;
+            swapCurrentPlayer();
             takeData();
-        }
+        } else System.out.println("Draw");
+
     }
 
-    public boolean didPlayerWin (char mark, int pos) {
+    private boolean didPlayerWin(char mark, int pos) {
         switch (pos) {
             case 4: // 4 pos to ensure
-                         if ( (map[0][0] == mark) && (map[2][2] == mark) ) return true;
-                    else if ( (map[0][1] == mark) && (map[2][1] == mark) ) return true;
-                    else if ( (map[0][2] == mark) && (map[2][0] == mark) ) return true;
-                else return ( (map[1][0] == mark) && (map[1][2] == mark) );
+                return (map[0][0] == mark) && (map[2][2] == mark) ||
+                        (map[0][1] == mark) && (map[2][1] == mark) ||
+                        (map[0][2] == mark) && (map[2][0] == mark) ||
+                        (map[1][0] == mark) && (map[1][2] == mark);
 
             case 0: // 3 pos to ensure
-                         if ( (map[0][1] == mark) && (map[0][2] == mark) ) return true;
-                    else if ( (map[1][0] == mark) && (map[2][0] == mark) ) return true;
-                else return ( (map[1][1] == mark) && (map[2][2] == mark) );
+                if ((map[0][1] == mark) && (map[0][2] == mark)) return true;
+                else if ((map[1][0] == mark) && (map[2][0] == mark)) return true;
+                else return ((map[1][1] == mark) && (map[2][2] == mark));
 
             case 2: // 3 pos to ensure
-                         if ( (map[0][0] == mark) && (map[0][1] == mark) ) return true;
-                    else if ( (map[1][2] == mark) && (map[2][2] == mark) ) return true;
-                else return ( (map[1][1] == mark) && (map[2][0] == mark) );
+                if ((map[0][0] == mark) && (map[0][1] == mark)) return true;
+                else if ((map[1][2] == mark) && (map[2][2] == mark)) return true;
+                else return ((map[1][1] == mark) && (map[2][0] == mark));
 
             case 6: // 3 pos to ensure
-                         if ( (map[0][0] == mark) && (map[1][0] == mark) ) return true;
-                    else if ( (map[2][1] == mark) && (map[2][2] == mark) ) return true;
-                else return ( (map[1][1] == mark) && (map[0][2] == mark) );
+                if ((map[0][0] == mark) && (map[1][0] == mark)) return true;
+                else if ((map[2][1] == mark) && (map[2][2] == mark)) return true;
+                else return ((map[1][1] == mark) && (map[0][2] == mark));
 
             case 8: // 3 pos to ensure
-                         if ( (map[0][2] == mark) && (map[1][2] == mark) ) return true;
-                    else if ( (map[2][0] == mark) && (map[2][1] == mark) ) return true;
-                else return ( (map[1][1] == mark) && (map[0][0] == mark) );
+                if ((map[0][2] == mark) && (map[1][2] == mark)) return true;
+                else if ((map[2][0] == mark) && (map[2][1] == mark)) return true;
+                else return ((map[1][1] == mark) && (map[0][0] == mark));
 
             case 1: // 2 pos to ensure
-                         if ( (map[0][0] == mark) && (map[0][2] == mark) ) return true;
-                else return ( (map[1][1] == mark) && (map[2][1] == mark) );
+                if ((map[0][0] == mark) && (map[0][2] == mark)) return true;
+                else return ((map[1][1] == mark) && (map[2][1] == mark));
             case 3: // 2 pos to ensure
-                         if ( (map[0][0] == mark) && (map[2][0] == mark) ) return true;
-                else return ( (map[1][1] == mark) && (map[1][2] == mark) );
+                if ((map[0][0] == mark) && (map[2][0] == mark)) return true;
+                else return ((map[1][1] == mark) && (map[1][2] == mark));
             case 5: // 2 pos to ensure
-                         if ( (map[0][2] == mark) && (map[2][2] == mark) ) return true;
-                else return ( (map[1][0] == mark) && (map[1][1] == mark) );
+                if ((map[0][2] == mark) && (map[2][2] == mark)) return true;
+                else return ((map[1][0] == mark) && (map[1][1] == mark));
             case 7: // 2 pos to ensure
-                         if ( (map[0][1] == mark) && (map[1][1] == mark) ) return true;
-                else return ( (map[2][0] == mark) && (map[2][2] == mark) );
-                default: return false;
+                if ((map[0][1] == mark) && (map[1][1] == mark)) return true;
+                else return ((map[2][0] == mark) && (map[2][2] == mark));
+            default:
+                return false;
         }
     }
 
-    public void takeData () {
-        System.out.println("Type data (e.g.: X 3), space is necessary");
+    private void takeData() {
+        System.out.println("Сейчас ходит " + currentPlayer);
         Scanner in = new Scanner(System.in);
-        String buf = "";
-        String[] masStr;
-        if (in.hasNextLine()) buf = in.nextLine();
-        masStr = buf.split(" "); // split strings by delimeter
-        if (masStr.length != 2) {
-            System.err.println("Wrong data!\ntry again");
-            takeData();
+        String str;
+        int pos;
+        while (true) {
+            str = in.nextLine(); // split strings by delimiter
+            try {
+                pos = Integer.valueOf(str);
+            } catch (NumberFormatException e) {
+                System.err.println("Wrong data!\nTry again");
+                continue;
+            }
+
+            if ((pos > 9) || (pos < 1)) {
+                System.err.println("Wrong position!\nType data again: ");
+                continue;
+            }
+            break;
         }
-        mark = masStr[0].charAt(0);
-        pos = Integer.valueOf(masStr[1]);
-        setMark(mark, pos);
+        setMark(pos);
     }
 }
